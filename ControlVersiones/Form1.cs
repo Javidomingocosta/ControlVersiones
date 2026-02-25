90e824e28c2325e5f70ce974ea8d339558d93eb2
JCV <JCV@DESKTOP-AMUNKSO>
2026-02-25 12:40:54 +0100

    commit

--- ControlVersiones/Form1.cs
+++ ControlVersiones/Form1.cs
@@ -16,7 +16,7 @@ namespace ControlVersiones
         {
             InitializeComponent();
         }
-
+        //test jcv
         private void button1_Click(object sender, EventArgs e)
         {
 
