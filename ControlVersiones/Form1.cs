3bc8340f09e4b9bac9456e764d7c0cdce5b4e969
JaviDomingo <javidomingo@digitechfp.com>
2026-02-25 12:41:08 +0100

    CAMBIO jdc

--- ControlVersiones/Form1.cs
+++ ControlVersiones/Form1.cs
@@ -15,6 +15,7 @@ namespace ControlVersiones
         public Form1()
         {
             InitializeComponent();
+            // HELOUU//
         }
 
         private void button1_Click(object sender, EventArgs e)
